07f1aa42dd591801e1122f9b861c9ef141f022e6
suramisam <surami.sam@gmail.com>
2021-07-09 19:03:32 +0530

    create regitration form

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "surami";
+
+$firstName=$_POST["FirstName"];
+$lastName=$_POST["LastName"];
+$gender=$_POST["Gender"];
+$dob=$_POST["dob"];
+$email=$_POST["email"];
+$address=$_POST["address"];
+$contactNo=$_POST["contactNo"];
+
+
+//Create connection
+$conn = new mysqli($servername, $username, $password,"school","3307");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$sql = "INSERT INTO student (first_name, last_name, gender,dob,email,address,contact_number) VALUES ('".$firstName."', '".$lastName."', '".$gender."', '".$dob."','".$email."','".$address."','".$contactNo."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
+
+<?php
+$variableee = readfile("registration.html");
+echo $variableee;
+?>
---
+++ students.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "surami";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"school","3307");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br>";
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
